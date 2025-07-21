fa7eb921ea2a72d60fb28df7892173d4e3759aef
Kyaw Min Oo <kyawmin.dac@gmail.com>
2025-07-21 22:02:27 +0630

    finished until func js100

---
+++ the_basics/basic.js
@@ -0,0 +1,12 @@
+console.log('Kyaw Min' + ' ' + 'Oo');
+console.log('5' + 10);
+console.log(`${parseInt('5') + 10}`);
+console.log(`The value of 5 + 10 is ${Number('5') + 10}.`);
+let names = ['asta', 'butterscotch', 'pudding', 'neptune', 'darwin'];
+let pets = {
+  asta: 'dog',
+  butterscotch: 'cat',
+  pudding: 'cat',
+  neptune: 'fish',
+  darwin: 'lizard',
+};
---
+++ variable/greeter.js
@@ -0,0 +1,4 @@
+let namer = 'Victor';
+console.log(`Good Morning, ${namer}.`);
+console.log(`Good Afternoon, ${namer}.`);
+console.log(`Good Evening, ${namer}.`);
